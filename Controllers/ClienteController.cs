b1c4ab78c08e4556d0307ca72713dce23c8630e7
Maria Clara <maria.clara@bcb.gov.br>
2024-10-03 19:34:49 -0300

    Adicionando edicao e exclusao

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Apsen.Controllers;
 
@@ -20,11 +21,25 @@ public class ClienteController : ControllerBase
     public IEnumerable<Cliente> GetClientes(){
         return _context.Clientes;
     }
-
+    [HttpGet("{cpnj}")]
+    public Cliente GetCliente(string cpnj){
+        return _context.Clientes.First(n => n.CNPJ == cpnj);
+    }
     [HttpPost(Name = "PostClient")]
     public void CreateCliente(Cliente cliente){
         _context.Clientes.Add(cliente);
         _context.SaveChanges();
 
+    }
+    [HttpPut("{cnpj}")]
+    public void CreateCliente(string cnpj, Cliente cliente){
+        _context.Entry(cliente).State = EntityState.Modified;
+        _context.SaveChanges();
+    }
+    [HttpDelete("{cnpj}")]
+    public void DeleteCliente(string cnpj){
+        Cliente clienteDeletar = _context.Clientes.Find(cnpj);
+        _context.Clientes.Remove(clienteDeletar);
+        _context.SaveChanges();
     }
 }
